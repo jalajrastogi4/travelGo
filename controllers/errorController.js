cf1c303f2cac8d09a741e59571442e114da4d083
Jalaj Rastogi <jalajrastogi4@gmail.com>
2020-01-29 08:46:06 +0530

    added different error handling for production and development env

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -68,7 +68,7 @@ const sendErrorProd = (err, req, res) => {
   // Operational, trusted error: send message to client
   if (err.isOperational) {
     return res.status(err.statusCode).render('error', {
-      title: 'Something went wrong!',
+      title: 'OOPS! Something looks wrong!',
       msg: err.message
     });
   }
@@ -77,7 +77,7 @@ const sendErrorProd = (err, req, res) => {
   console.error('ERROR 💥', err);
   // Sending generic message
   return res.status(err.statusCode).render('error', {
-    title: 'Something went wrong!',
+    title: 'OOPS! Something looks wrong!',
     msg: 'Please try again later.'
   });
 };
